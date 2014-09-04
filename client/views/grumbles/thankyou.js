1cbecbab679f65db57ae7ced3edcedc4b45d194e
eyha <psyea2@nottingham.ac.uk>
2014-09-04 09:45:39 +0100

    Added a button on the thankyou page to submit another issue

--- client/views/grumbles/thankyou.js
+++ client/views/grumbles/thankyou.js
@@ -2,3 +2,15 @@ Template.submit.rendered = function() {
 	// alert("scroll Called");
 	jQuery('html','body').animate({scrollTop:0},0);
 }
+
+Template.submit.helpers({
+	getURL: function() {
+		if(Session.get('form') && (Session.get('id'))){
+			var url = location.protocol + '//' + location.hostname + (location.port && ":" + location.port) + "/" + Session.get('form') + '/' + Session.get('id');
+			return url;
+		}
+		else{
+			return '#';
+		}
+	}
+})
